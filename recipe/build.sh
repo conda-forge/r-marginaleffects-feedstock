a7973e31270bd01ffc56407670adb91a71be5920
Mervin Fansler <mef3005@med.cornell.edu>
2023-03-13 15:22:44 -0400

    log1p

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,9 @@
 
 export DISABLE_AUTOBREW=1
 
+## needed for Arch builds
+## see https://github.com/conda-forge/r-base-feedstock/issues/163
+export PKG_CPPFLAGS="-DHAVE_WORKING_LOG1P"
+
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
